fc4b0c6f9c1a6c5c563e2bfdb0dd481769ea24c8
Theodore Cooper <63190431+theodorecooper@users.noreply.github.com>
2022-12-16 23:15:13 +0800

    random background

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,4 +1,9 @@
 extends Node
-
+	
 func _ready():
 	GameData.score = 0
+	var bg = [
+		preload("res://assets/sprites/background-day.png"),
+		preload("res://assets/sprites/background-night.png")
+	]
+	$Background.set_texture(bg[randi() % 2])
